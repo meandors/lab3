ab9ca8bf654f27d6bea4952d32aecceb054cdac9
meandors <meandors@tlen.pl>
2016-04-11 22:12:21 +0200

    Add project files.

---
+++ Lab4/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbPolski = new System.Windows.Forms.RadioButton();
+            this.rbAngielski = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tbWyszukaj = new System.Windows.Forms.TextBox();
+            this.tbWynik = new System.Windows.Forms.TextBox();
+            this.tbToTranslate = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rbPolski
+            // 
+            this.rbPolski.AutoSize = true;
+            this.rbPolski.Checked = true;
+            this.rbPolski.Location = new System.Drawing.Point(37, 28);
+            this.rbPolski.Name = "rbPolski";
+            this.rbPolski.Size = new System.Drawing.Size(60, 17);
+            this.rbPolski.TabIndex = 0;
+            this.rbPolski.TabStop = true;
+            this.rbPolski.Text = "pol-ang";
+            this.rbPolski.UseVisualStyleBackColor = true;
+            this.rbPolski.CheckedChanged += new System.EventHandler(this.rbAngielski_CheckedChanged);
+            // 
+            // rbAngielski
+            // 
+            this.rbAngielski.AutoSize = true;
+            this.rbAngielski.Location = new System.Drawing.Point(201, 28);
+            this.rbAngielski.Name = "rbAngielski";
+            this.rbAngielski.Size = new System.Drawing.Size(66, 17);
+            this.rbAngielski.TabIndex = 1;
+            this.rbAngielski.Text = "ang - pol";
+            this.rbAngielski.UseVisualStyleBackColor = true;
+            this.rbAngielski.CheckedChanged += new System.EventHandler(this.rbAngielski_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(37, 107);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // tbWyszukaj
+            // 
+            this.tbWyszukaj.Location = new System.Drawing.Point(37, 81);
+            this.tbWyszukaj.Name = "tbWyszukaj";
+            this.tbWyszukaj.Size = new System.Drawing.Size(120, 20);
+            this.tbWyszukaj.TabIndex = 3;
+            this.tbWyszukaj.TextChanged += new System.EventHandler(this.tbWyszukaj_TextChanged);
+            // 
+            // tbWynik
+            // 
+            this.tbWynik.Location = new System.Drawing.Point(201, 107);
+            this.tbWynik.Multiline = true;
+            this.tbWynik.Name = "tbWynik";
+            this.tbWynik.Size = new System.Drawing.Size(208, 95);
+            this.tbWynik.TabIndex = 5;
+            // 
+            // tbToTranslate
+            // 
+            this.tbToTranslate.Location = new System.Drawing.Point(201, 80);
+            this.tbToTranslate.Name = "tbToTranslate";
+            this.tbToTranslate.Size = new System.Drawing.Size(100, 20);
+            this.tbToTranslate.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(307, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 25);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "T";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(343, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 25);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(379, 75);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(30, 25);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 240);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tbToTranslate);
+            this.Controls.Add(this.tbWynik);
+            this.Controls.Add(this.tbWyszukaj);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.rbAngielski);
+            this.Controls.Add(this.rbPolski);
+            this.Name = "Form1";
+            this.Text = "Słownik";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbPolski;
+        private System.Windows.Forms.RadioButton rbAngielski;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox tbWyszukaj;
+        private System.Windows.Forms.TextBox tbWynik;
+        private System.Windows.Forms.TextBox tbToTranslate;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Lab4/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        private SortedDictionary<string, string> polski;
+        private SortedDictionary<string, string> angielski;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+            polski = new SortedDictionary<string, string>();
+            angielski = new SortedDictionary<string, string>();
+
+            angielski.Add("cat", "dog");
+            polski.Add("kot", "cat");
+            angielski.Add("dog", "pies");
+            polski.Add("pies", "dog");
+            angielski.Add("wolf", "wilk");
+            polski.Add("wilk", "wolf");
+
+            foreach (var item in polski)
+            {
+                listBox1.Items.Add(item.Key);
+            }
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            tbToTranslate.Text = listBox1.Text;
+            tbWynik.Text = "";
+        }
+
+        private void tbWyszukaj_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (rbPolski.Checked)
+            {
+                foreach (var item in polski)
+                {
+                    string word = item.Key;
+                    if (word.Contains(tbWyszukaj.Text))
+                    {
+                        listBox1.Items.Add(word);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var item in angielski)
+                {
+                    string word = item.Key;
+                    if (word.Contains(tbWyszukaj.Text))
+                    {
+                        listBox1.Items.Add(word);
+                    }
+                }
+            }
+        }
+
+        private void rbAngielski_CheckedChanged(object sender, EventArgs e)
+        {
+            reFillList();
+//            listBox1.Items.Clear();
+//            tbToTranslate.Text = "";
+//            tbWynik.Text = "";
+//            tbWyszukaj.Text = "";
+//            if (rbPolski.Checked)
+//            {
+//                foreach (var item in polski)
+//                {
+//                    listBox1.Items.Add(item.Key);
+//                }
+//            }
+//            else
+//            {
+//                foreach (var item in angielski)
+//                {
+//                    listBox1.Items.Add(item.Key);
+//                }
+//            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tbToTranslate.Text != "")
+            {
+                if (rbPolski.Checked)
+                {
+                    if (polski.ContainsKey(tbToTranslate.Text))
+                    {
+                        tbWynik.Text = polski[tbToTranslate.Text];
+                    }
+                    else
+                    {
+                        MessageBox.Show("Brak słowa w lowniku!");
+                    }
+                }
+                else
+                {
+                    if (angielski.ContainsKey(tbToTranslate.Text))
+                    {
+                        tbWynik.Text = angielski[tbToTranslate.Text];
+                    }
+                    else
+                    {
+                        MessageBox.Show("Brak słowa w lowniku!");
+                    }
+                
+                }
+                
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (tbToTranslate.Text == "" || tbWynik.Text == "") return;
+            if (rbPolski.Checked)
+            {
+                if (!polski.ContainsKey(tbToTranslate.Text))
+                {
+                    polski.Add(tbToTranslate.Text, tbWynik.Text);
+                    angielski.Add(tbWynik.Text, tbToTranslate.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Słowo juz wystepuje w słowniku!");
+                }
+            }
+            else
+            {
+                if (!angielski.ContainsKey(tbToTranslate.Text))
+                {
+                    angielski.Add(tbToTranslate.Text, tbWynik.Text);
+                    polski.Add(tbWynik.Text, tbToTranslate.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Word already exist in dictionary!");
+                }
+            }
+            tbToTranslate.Text = "";
+            tbWynik.Text = "";
+            reFillList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (tbToTranslate.Text == "") return;
+            if (rbPolski.Checked)
+            {
+                if (polski.ContainsKey(tbToTranslate.Text))
+                {
+                    angielski.Remove(polski[tbToTranslate.Text]);
+                    polski.Remove(tbToTranslate.Text);
+                }
+            }
+            else
+            {
+                if (angielski.ContainsKey(tbToTranslate.Text))
+                {
+                    polski.Remove(angielski[tbToTranslate.Text]);
+                    angielski.Remove(tbToTranslate.Text);
+                }
+            }
+            tbToTranslate.Text = "";
+            tbWynik.Text = "";
+            reFillList();
+        }
+
+        private void reFillList()
+        {
+            listBox1.Items.Clear();
+            tbToTranslate.Text = "";
+            tbWynik.Text = "";
+            tbWyszukaj.Text = "";
+            if (rbPolski.Checked)
+            {
+                foreach (var item in polski)
+                {
+                    listBox1.Items.Add(item.Key);
+                }
+            }
+            else
+            {
+                foreach (var item in angielski)
+                {
+                    listBox1.Items.Add(item.Key);
+                }
+            }
+        }
+    }
+}
